01fe13f15b6398aaaaa00b9e4f4e284e47b25bc1
JoaquinRG00 <joaquinr.industrial@gmail.com>
2024-07-15 18:02:49 +0200

    Rename Media_Movil_Q_Unroll2.cpp to Media_Movil_Unroll2.cpp

--- VitisHLS/Media_Movil/Media_Movil_Unroll/Media_Movil_Unroll2.cpp
+++ VitisHLS/Media_Movil/Media_Movil_Unroll/Media_Movil_Unroll2.cpp
@@ -1,5 +1,5 @@
-// CÛdigo de la Media Movil con ap_fixed (con cuantizacion)
-// JoaquÌn Ramos GarcÌa
+// C√≥digo de la Media Movil con ap_fixed (con cuantizacion)
+// Joaqu√≠n Ramos Garc√≠a
 
 /*
 #include <stdio.h>
@@ -24,7 +24,7 @@ Q_data shift_reg_2[Nsamples/4] = {0};//Se inicializa como un vector de 16 ceros
 Q_data shift_reg_3[Nsamples/4] = {0};//Se inicializa como un vector de 16 ceros
 Q_data shift_reg_4[Nsamples/4] = {0};//Se inicializa como un vector de 16 ceros
 //static Q_suma acumulado = 0;
-// RNC: Esta bien, pero para que sea m·s escalable vamos a trabajar con ventanas de 64 para obtener una sola muestra de salida. la entrada no puede ser const
+// RNC: Esta bien, pero para que sea m√°s escalable vamos a trabajar con ventanas de 64 para obtener una sola muestra de salida. la entrada no puede ser const
 void media_movil(Q_data input[1], Q_data output[1]) {
 
 	Q_suma_16 acumulado_1 = 0;
@@ -32,7 +32,7 @@ void media_movil(Q_data input[1], Q_data output[1]) {
 	Q_suma_16 acumulado_3 = 0;
 	Q_suma_16 acumulado_4 = 0;
 	Q_suma_64 acumulado = 0;
-	// RNC: Debemos definir la seÒal EMG_rectificada. No hace falta que sea un vector, porque usamos el valor y sobreescribimos. AsÌ ahorramos memoria.
+	// RNC: Debemos definir la se√±al EMG_rectificada. No hace falta que sea un vector, porque usamos el valor y sobreescribimos. As√≠ ahorramos memoria.
 	Q_data EMG_rectificada_1;
 	Q_data EMG_rectificada_2;
 	Q_data EMG_rectificada_3;
@@ -60,25 +60,25 @@ void media_movil(Q_data input[1], Q_data output[1]) {
 	for_media_movil:for (int k=0; k < Nsamples/4; k++)
 	{
 
-		// Hacemos el valor absoluto de la seÒal, del registro de desplazamiento 1
+		// Hacemos el valor absoluto de la se√±al, del registro de desplazamiento 1
 		if(shift_reg_1[k] < 0){
 			EMG_rectificada_1 = -shift_reg_1[k];
 		}else{
 			EMG_rectificada_1 = shift_reg_1[k];
 		}
-		// Hacemos el valor absoluto de la seÒal, del registro de desplazamiento 2
+		// Hacemos el valor absoluto de la se√±al, del registro de desplazamiento 2
 		if(shift_reg_2[k] < 0){
 			EMG_rectificada_2 = -shift_reg_2[k];
 		}else{
 			EMG_rectificada_2 = shift_reg_2[k];
 		}
-		// Hacemos el valor absoluto de la seÒal, del registro de desplazamiento 3
+		// Hacemos el valor absoluto de la se√±al, del registro de desplazamiento 3
 		if(shift_reg_3[k] < 0){
 			EMG_rectificada_3 = -shift_reg_3[k];
 		}else{
 			EMG_rectificada_3 = shift_reg_3[k];
 		}
-		// Hacemos el valor absoluto de la seÒal, del registro de desplazamiento 4
+		// Hacemos el valor absoluto de la se√±al, del registro de desplazamiento 4
 		if(shift_reg_4[k] < 0){
 			EMG_rectificada_4 = -shift_reg_4[k];
 		}else{
@@ -92,7 +92,7 @@ void media_movil(Q_data input[1], Q_data output[1]) {
 
 	acumulado = acumulado_1+acumulado_2+acumulado_3+acumulado_4;
 	// printf("acum = %f\n\r", acumulado);
-	//La divisiÛn est· bien, pero al ser potencia de 2 puedes hacer un desplazamiento a la derecha para implementar la divisiÛn.
+	//La divisi√≥n est√° bien, pero al ser potencia de 2 puedes hacer un desplazamiento a la derecha para implementar la divisi√≥n.
 	output[0] = acumulado / Nsamples;
 	// output = (acumulado >> Nshift);
 
